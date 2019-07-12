35ce57e706abeef0cb4eecb278e0fde37d00b3b3
khanhdrag9 <tkhanh783@gmail.com>
2019-07-12 17:55:57 +0700

    - update gen map

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -9,17 +9,22 @@ public class Map : MonoBehaviour
     [SerializeField] float end = 7;
     [SerializeField] Vector2 distance = new Vector2(0.5f, 0.5f);
     [SerializeField] Color[] colors;
+    [SerializeField] int startXMin;
+    [SerializeField] int startXMax;
+    [SerializeField] int highMin;
+    [SerializeField] int highMax;
+    
     int startColor = 0;
     int startOrder = 100;
+    int startLine = 0;
 
     class Stair
     {
         public List<GameObject> stairList = null;
-        public List<GameObject> wall = null;
+        public GameObject wall = null;
         public Stair()
         {
             stairList = new List<GameObject>();
-            wall = new List<GameObject>();
         }
     }
 
@@ -27,14 +32,16 @@ public class Map : MonoBehaviour
 
     void Start()
     {
-        int numberStair = 7;
-        int startLine = 0;
+        int numberStair = 15;
         for(int i = 0; i < numberStair; i++)
         {
-            int startX = Random.Range(0, 1);
-            int high = Random.Range(3, 3);
+            int startX = Random.Range(startXMin, startXMax);
+            int high = Random.Range(highMin, highMax);
             Direction direction = i % 2 == 0 ? Direction.LEFT : Direction.RIGHT;
-            GenerateStairs(direction, startLine, startX, high, distance);
+            GenerateStairs(direction, startLine, startX, high, startColor, startOrder, distance);
+
+            startColor++;
+            startOrder--;
             startLine += high;
         }
     }
@@ -44,7 +51,7 @@ public class Map : MonoBehaviour
         
     }
 
-    Stair GenerateStairs(Direction from, float startLine, float startX, float high, Vector2 rate)
+    Stair GenerateStairs(Direction from, int startLine, float startX, int high, int colorIndex, int order, Vector2 rate)
     {
         Stair stair = new Stair();
         int add = (from == Direction.LEFT ? -1 : 1);
@@ -52,14 +59,14 @@ public class Map : MonoBehaviour
         {
             float x = startX + i * add;
             float y = startLine + i;
-            GameObject square = SpawnSquare(from, new Vector2(x, y), GetColor(startColor), startOrder, rate);
+            GameObject square = SpawnSquare(from, new Vector2(x, y), GetColor(colorIndex), startOrder, rate);
             stair.stairList.Add(square);
-
-            GameObject wall = SpawnSquare(Direction.RIGHT, new Vector2(this.start * 2, y), GetColor(startColor + 1), startOrder - 1, rate);
-            stair.wall.Add(wall);
         }
-        startColor++;
-        startOrder--;
+        GameObject wall = SpawnSquare(Direction.RIGHT, new Vector2(this.start / rate.x, startLine), GetColor(colorIndex + 1), startOrder - 1, rate);
+        wall.transform.localScale += new Vector3(0, high - 1, 0);
+
+        stair.wall = wall;
+
         return stair;
     }
 
